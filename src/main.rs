a6732e7ced43d591d75a6ed4e456db7435ea9019
Lucas <lucasclaudinocc@gmail.com>
2023-12-28 17:39:45 -0300

    Added base function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Adivinhe o número!");
+
+    println!("Por favor, insira seu palpite.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Falha ao ler a linha");
+
+    println!("Você digitou: {guess}");
+
+    let mut dablio = String::new();
+    io::stdin()
+        .read_line(&mut dablio)
+        .expect("Falha ao ler a linha");
+    println!("Você digitou: {dablio}");
 }
